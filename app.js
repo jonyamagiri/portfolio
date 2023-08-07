30b667dcdee5f6a8ae580c42ebcfed457a6c2166
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-08-07 18:48:46 +0300

    sticky-nav and no-scroll

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
+    body.classList.toggle('no-scroll')
 });
+
+// sticky nav
+
+const nav = document.querySelector('nav');
+
+function handleScroll() {
+    if (window.scrollY > 0) {
+        nav.classList.add('sticky-nav')
+    } else {
+        nav.classList.remove('sticky-nav')
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
